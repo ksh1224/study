50b21d1d7cf5b7d5169bd8a07e328efa268d3ff9
권수현 <3a9lyd@naver.com>
2021-02-22 01:29:54 +0900

    커스텀 모달 추가

---
+++ src/components/elements/CustomModal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Modal, Pressable } from 'react-native';
+import { background, styles } from 'utils/stylesUtil';
+
+interface Props {
+  visible?: boolean;
+  close?: () => void;
+  children: JSX.Element;
+}
+
+export default function CustomModal({
+  visible = false,
+  close = () => {},
+  children,
+}: Props) {
+  return (
+    <Modal visible={visible} transparent>
+      <View style={styles(['flex'])}>
+        <Pressable
+          onPress={close}
+          style={styles(['flex', 'center', background('#0005')])}>
+          <Pressable onPress={() => {}}>{children}</Pressable>
+        </Pressable>
+      </View>
+    </Modal>
+  );
+}
