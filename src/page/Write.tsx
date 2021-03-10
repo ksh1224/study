5616aefe6ccdbdeeb19190d91bad2db3245b847f
yeojin <jeonyeojin1993@gmail.com>
2021-03-10 20:41:11 +0900

    [feat] page 6 초기작업 -- page 6 초기작업

--- src/page/Write.tsx
+++ src/page/Write.tsx
@@ -1,104 +1,91 @@
 import { useNavigation } from '@react-navigation/native';
 import Header from 'components/section/Header';
 import React from 'react';
-import { Pressable, Text, View, TextInput } from 'react-native';
-import {
-  customColors,
-  padding,
-  styles,
-  text,
-  height,
-  border,
-  margin,
-} from 'utils/stylesUtil';
+import { Pressable, Text, View, StyleSheet, ScrollView, Image } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { customColors, REM } from 'utils/stylesUtil';
+import { Picker } from '@react-native-picker/picker';
+
+
+const pictureSize = 200;
 
 export default function Write() {
   const navigation = useNavigation<RootStackkNavigationProps>();
 
-  const box = styles([
-    { width: '100%' },
-    height(43),
-    border({ color: 'rgb(198, 198, 198)', radius: 11, width: 1 }),
-    margin({ bottom: 20 }),
-  ]);
-
   return (
-    <View style={styles(['flex'])}>
+    <View style={styles.flex}>
       <Header
         back={() => {
           navigation.goBack();
         }}
         rightButton={
           <Pressable onPress={() => console.log('update')}>
-            <Text
-              style={styles([
-                text({ size: 18, color: customColors.main, weight: 'Bold' }),
-              ])}>
-              생성
-            </Text>
+            <Text style={styles.buttonText}>생성</Text>
           </Pressable>
         }
       />
-      <View style={styles(['flex', padding(20)])}>
-        <TextInput style={box} placeholder="이름" />
-        <View style={box}>
-          <Text>ssss</Text>
+
+      <View style={styles.pictureWrap}>
+        <ScrollView horizontal>
+          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
+          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
+          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
+          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
+          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
+
+        </ScrollView>
+      </View>
+      
+      <View style={styles.body}>
+        <TextInput style={styles.box} placeholder="상품명"/>
+
+        {/*  주소 */}
+
+        <View style={[styles.box, { padding: 0 }]}>
+          <Picker style={styles.flex}>
+            <Picker.Item label="테스트" value="test" />
+            <Picker.Item label="카테고리" value={0} />
+          </Picker>
+        </View>
+                
+        <View style={styles.box}>
+          <Text>이름</Text>
         </View>
       </View>
     </View>
   );
 }
 
-// import { useNavigation } from '@react-navigation/native';
-// import Header from 'components/section/Header';
-// import React from 'react';
-// import { Pressable, Text, View, StyleSheet } from 'react-native';
-// import { TextInput } from 'react-native-gesture-handler';
-// import { customColors, REM } from 'utils/stylesUtil';
-
-// export default function Write() {
-//   const navigation = useNavigation<RootStackkNavigationProps>();
-
-//   return (
-//     <View style={styles.flex}>
-//       <Header
-//         back={() => {
-//           navigation.goBack();
-//         }}
-//         rightButton={
-//           <Pressable onPress={() => console.log('update')}>
-//             <Text style={styles.buttonText}>생성</Text>
-//           </Pressable>
-//         }
-//       />
-//       <View style={styles.body}>
-//         <TextInput />
-//         <View style={styles.box}>
-//           <Text>ssss</Text>
-//         </View>
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   flex: {
-//     flex: 1,
-//   },
-//   buttonText: {
-//     fontSize: 18 * REM,
-//     color: customColors.main,
-//     fontFamily: 'Bold',
-//   },
-//   body: {
-//     flex: 1,
-//     padding: 20 * REM,
-//   },
-//   box: {
-//     width: '100%',
-//     height: 43 * REM,
-//     borderColor: 'rgb(198,198,198)',
-//     borderWidth: 1 * REM,
-//     borderRadius: 11 * REM,
-//   },
-// });
+const styles = StyleSheet.create({
+  
+  flex: {
+    flex: 1,
+  },
+  buttonText: {
+    fontSize: 18 * REM,
+    color: customColors.main,
+    fontFamily: 'Bold',
+  },
+  body: {
+    flex: 1,
+    padding: 20 * REM,
+  },
+  box: {
+    width: '100%',
+    height: 43 * REM,
+    borderColor: 'rgb(198,198,198)',
+    borderWidth: 1 * REM,
+    borderRadius: 11 * REM,
+    marginBottom: 20 * REM,
+    padding: 10 * REM
+  },
+  pictureWrap: {
+    width: '100%',
+    height: pictureSize * REM
+  },
+  picture: {
+    width: pictureSize * REM,
+    height: pictureSize * REM,
+    borderRadius: 11 * REM,
+  }
+});
