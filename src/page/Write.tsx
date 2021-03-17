c9e154d60c52dbd8cbfede1a8a7576a9f69d5589
권수현 <3a9lyd@naver.com>
2021-03-17 22:38:04 +0900

    린트 수정

--- src/page/Write.tsx
+++ src/page/Write.tsx
@@ -1,11 +1,17 @@
 import { useNavigation } from '@react-navigation/native';
 import Header from 'components/section/Header';
 import React from 'react';
-import { Pressable, Text, View, StyleSheet, ScrollView, Image } from 'react-native';
+import {
+  Pressable,
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { customColors, REM } from 'utils/stylesUtil';
 import { Picker } from '@react-native-picker/picker';
-
 
 const pictureSize = 200;
 
@@ -27,17 +33,46 @@ export default function Write() {
 
       <View style={styles.pictureWrap}>
         <ScrollView horizontal>
-          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
-          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
-          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
-          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
-          <Image style={styles.picture} source={{uri:"http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"}} />
-
+          <Image
+            style={styles.picture}
+            source={{
+              uri:
+                'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+            }}
+          />
+          <Image
+            style={styles.picture}
+            source={{
+              uri:
+                'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+            }}
+          />
+          <Image
+            style={styles.picture}
+            source={{
+              uri:
+                'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+            }}
+          />
+          <Image
+            style={styles.picture}
+            source={{
+              uri:
+                'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+            }}
+          />
+          <Image
+            style={styles.picture}
+            source={{
+              uri:
+                'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+            }}
+          />
         </ScrollView>
       </View>
-      
+
       <View style={styles.body}>
-        <TextInput style={styles.box} placeholder="상품명"/>
+        <TextInput style={styles.box} placeholder="상품명" />
 
         {/*  주소 */}
 
@@ -47,7 +82,7 @@ export default function Write() {
             <Picker.Item label="카테고리" value={0} />
           </Picker>
         </View>
-                
+
         <View style={styles.box}>
           <Text>이름</Text>
         </View>
@@ -57,7 +92,6 @@ export default function Write() {
 }
 
 const styles = StyleSheet.create({
-  
   flex: {
     flex: 1,
   },
@@ -77,15 +111,15 @@ const styles = StyleSheet.create({
     borderWidth: 1 * REM,
     borderRadius: 11 * REM,
     marginBottom: 20 * REM,
-    padding: 10 * REM
+    padding: 10 * REM,
   },
   pictureWrap: {
     width: '100%',
-    height: pictureSize * REM
+    height: pictureSize * REM,
   },
   picture: {
     width: pictureSize * REM,
     height: pictureSize * REM,
     borderRadius: 11 * REM,
-  }
+  },
 });
